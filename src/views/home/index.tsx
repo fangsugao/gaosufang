ad869e2a5896778ccf9b5c31bf3903e1c898ef9f
fangsugao <1914535174@qq.com>
2019-09-04 14:13:14 +0800

    xiu home

--- src/views/home/index.tsx
+++ src/views/home/index.tsx
@@ -52,10 +52,10 @@ class Home extends React.Component {
               </span>
             }
           >
-            <Menu.Item key="1">Option 1</Menu.Item>
-            <Menu.Item key="2">Option 2</Menu.Item>
-            <Menu.Item key="3">Option 3</Menu.Item>
-            <Menu.Item key="4">Option 4</Menu.Item>
+            <Menu.Item key="1">添加试题</Menu.Item>
+            <Menu.Item key="2">试题分类</Menu.Item>
+            <Menu.Item key="3">查看试题</Menu.Item>
+
           </SubMenu>
           <SubMenu
             key="sub2"
@@ -66,8 +66,8 @@ class Home extends React.Component {
               </span>
             }
           >
-            <Menu.Item key="5">Option 5</Menu.Item>
-            <Menu.Item key="6">Option 6</Menu.Item>
+            <Menu.Item key="5">添加用户</Menu.Item>
+            <Menu.Item key="6">用户展示</Menu.Item>
             <SubMenu key="sub3" title="Submenu">
               <Menu.Item key="7">Option 7</Menu.Item>
               <Menu.Item key="8">Option 8</Menu.Item>
@@ -82,10 +82,9 @@ class Home extends React.Component {
               </span>
             }
           >
-            <Menu.Item key="9">Option 9</Menu.Item>
-            <Menu.Item key="10">Option 10</Menu.Item>
-            <Menu.Item key="11">Option 11</Menu.Item>
-            <Menu.Item key="12">Option 12</Menu.Item>
+            <Menu.Item key="9">添加考试</Menu.Item>
+            <Menu.Item key="10">试卷管理</Menu.Item>
+          
           </SubMenu>
         </Menu>
       </div>
